0c2175a1a68dfbfbf68e2c4943f3637da21298f2
Brett <gamer.bah@live.com>
2020-02-07 13:21:17 -0800

    Initial commit for project structure

---
+++ src/main/java/org/flockofseagles/TriviaMaze.java
@@ -0,0 +1,9 @@
+package org.flockofseagles;
+
+public class TriviaMaze {
+
+	public static void main(String[] args) {
+		// Auto-generated stub
+	}
+
+}
---
+++ src/test/java/org/flockofseagles/TriviaMazeTest.java
@@ -0,0 +1,20 @@
+package org.flockofseagles;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class TriviaMazeTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
