64830987764c22b274c5f14ba67a9145b466f029
Doug Doner <ddoner02@gmail.com>
2020-03-17 18:31:24 -0700

    Properly parses HTML-escaped characters in JSON response.

--- src/main/java/org/flockofseagles/util/APIQuestionLoader.java
+++ src/main/java/org/flockofseagles/util/APIQuestionLoader.java
@@ -1,22 +1,19 @@
 package org.flockofseagles.util;
 
-import edu.emory.mathcs.backport.java.util.Arrays;
+import org.apache.commons.text.StringEscapeUtils;
 import org.flockofseagles.Question;
-import org.json.*;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
-import java.io.*;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
 import java.net.URI;
-import java.net.URISyntaxException;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-import java.nio.Buffer;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.nio.file.StandardOpenOption;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 
 public class APIQuestionLoader {
 
@@ -28,6 +25,7 @@ public class APIQuestionLoader {
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create(uri))
                 .header("Accept", "application/json")
+                .header("Content-Type", "text/plain;charset=UTF-8")
                 .build();
 
         HttpResponse<String> response =
@@ -39,16 +37,17 @@ public class APIQuestionLoader {
 
             responseArray.forEach(item -> {
                 JSONObject jsonItem = (JSONObject) item;
-                String questionString = jsonItem.getString("question") + "\n";
+
+                String questionString = StringEscapeUtils.unescapeHtml4(jsonItem.getString("question")) + "\n";
 
                 List<String> possibleAnswers = new ArrayList<String>();
 
-                possibleAnswers.add(jsonItem.getString("correct_answer") + "\n");
+                possibleAnswers.add(StringEscapeUtils.unescapeHtml4(jsonItem.getString("correct_answer")) + "\n");
 
                 JSONArray answersArray = jsonItem.getJSONArray("incorrect_answers");
 
                 for (Object o : answersArray)
-                    possibleAnswers.add((String) o + "\n");
+                    possibleAnswers.add(StringEscapeUtils.unescapeHtml4((String) o) + "\n");
 
                 addQuestionToFile(questionString, possibleAnswers);
 
@@ -62,9 +61,9 @@ public class APIQuestionLoader {
     private void addQuestionToFile(String questionText, List<String> possibleAnswers) {
 
         try {
-                FileOutputStream fos = new FileOutputStream("questions.txt", true);
-                OutputStreamWriter writer = new OutputStreamWriter(fos);
-                writer.append(questionText);
+            FileOutputStream fos = new FileOutputStream("questions.txt", true);
+            OutputStreamWriter writer = new OutputStreamWriter(fos);
+            writer.append(questionText);
 
             for (String s : possibleAnswers) {
                 writer.append(s);
